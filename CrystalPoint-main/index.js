148d50e3ca61f72719532936f3e7a8d46b396795
Ahmed Almadami <almadamiahmed@gmail.com>
2022-12-13 19:33:32 -0500

    first commit

---
+++ CrystalPoint-main/index.js
@@ -0,0 +1,23 @@
+function login() {
+  window.location.href = "login.html";
+}
+function story() {
+  window.location.href = "StoryPage.html";
+}
+
+function game() {
+  window.location.href = "mapLevel.html";
+}
+
+function mapLevel1() {
+  window.location.href = "mapLevel1.html";
+}
+
+function mapLevel2() {
+  window.location.href = "mapLevel2.html";
+}
+
+function mapLevel3() {
+  window.location.href = "mapLevel3.html";
+}
+
